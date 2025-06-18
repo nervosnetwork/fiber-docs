1c01f6dabfaddbb0b6c515e37c7908b1e9162e72
RetricSu <inneverland2013@gmail.com>
2025-06-18 18:15:16 +0800

    Doing some chore (#11)
    * fix: add favicon in metadata
    
    * chore: update some author info

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,17 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/logo/favicon.svg',
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
