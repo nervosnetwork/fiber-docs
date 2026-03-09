ae9bdf9b6b45e90848976db43bd5130d1dbf34ec
Sonny <happy.sonny.dev@gmail.com>
2026-03-09 09:18:09 +0800

    fix ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
         <HeroTitle />
         <NetworkStats />
         <HowItWorks />
-        <LiveSimulation />
+        {/* <LiveSimulation /> */}
         <NetworkComparison />
         <RocketAnimationSection />
         {/* <HighlightedFeatures /> */}
